24e3fa9eae26baed667aa8e8a29373c0bdec83a2
Paulo Kurtz <100733639+kurtzpmk@users.noreply.github.com>
2023-06-15 19:18:16 -0300

    update

--- Train.py
+++ Train.py
@@ -4,12 +4,12 @@ from Env import Env
 from RLAgent import RLAgent
 
 if __name__ == "__main__":
-    env = Env()
-    evaluationEnv = Env()
-    evaluationEnv = Monitor(evaluationEnv)
-    agent = RLAgent(env)
-    agent.trainAgent(1000000, evaluationEnv)
-
     # env = Env()
-    # agent = RLAgent(env, "model/A2C.zip")
-    # agent.runTrainedAgent(env, 10)
+    # evaluationEnv = Env()
+    # evaluationEnv = Monitor(evaluationEnv)
+    # agent = RLAgent(env)
+    # agent.trainAgent(1000000, evaluationEnv)
+
+    env = Env()
+    agent = RLAgent(env, "model/A2C.zip")
+    agent.runTrainedAgent(env, 10)
